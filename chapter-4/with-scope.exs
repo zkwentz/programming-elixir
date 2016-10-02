04e509159d41298d82c6fe143448a03e78c4784a
Zach Wentz <zkwentz@gmail.com>
2016-10-01 21:47:06 -0700

    better error handling for regex in with

--- chapter-4/with-scope.exs
+++ chapter-4/with-scope.exs
@@ -3,7 +3,7 @@ content = "Now is the time"
 lp = with {:ok, file} = File.open("/etc/passwd"),
        content       = IO.read(file,:all),
        :ok           = File.close(file),
-       [_, uid, gid] = Regex.run(~r/_lp:.*?:(\d+):(\d+)/,content)
+       [_, uid, gid] <- Regex.run(~r/_lp:.*?:(\d+):(\d+)/,content)
      do
        "Group: #{gid}, User: #{uid}"
      end
